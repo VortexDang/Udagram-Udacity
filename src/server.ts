a890e0a8740be5da9d7be0a7ca1b6c979193f261
Hoai Nam <hoainamnguyennhu7@gmail.com>
2022-11-29 16:14:42 +0700

    refactor(develop): add typing for req and res

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
@@ -27,7 +27,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: Request, res: Response) => {
     const image_url: string = req.query.image_url;
     const isValidHttpUrl = (inp: string) => {
       let url;
@@ -55,7 +55,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}");
   });
 
